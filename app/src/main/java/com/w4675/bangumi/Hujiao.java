e50c4f2c019c8d0aa30b4fa819afaf3004b61100
weiwuyanghou <l>
2019-05-15 16:03:50 +0800

    update

--- app/src/main/java/com/w4675/bangumi/Hujiao.java
+++ app/src/main/java/com/w4675/bangumi/Hujiao.java
@@ -1,5 +1,7 @@
 package com.w4675.bangumi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +9,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class Hujiao extends Fragment {
 
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment3,container,false);
+        ImageView imageView=view.findViewById(R.id.i1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:18722350063");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
